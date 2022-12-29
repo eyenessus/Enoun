5ce0d539a752915d54e46aeb2022de03215462f6
Emerson S <eyenessus@gmail.com>
2022-12-29 14:56:22 -0300

    conexao com banco de dados e tabelas do banco de dados criada

---
+++ Enoun/database/migrations/2022_12_29_171736_create_servicos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',100);
+            $table->text('descricao');
+            $table->string('categoria');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicos');
+    }
+}
