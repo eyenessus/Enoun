67265936cfad1f56760caba317d3b770badc7d79
Emerson S <eyenessus@gmail.com>
2022-12-29 22:15:09 -0300

    HTML Contato adicionado

--- Enoun/resources/views/Contato/contato.blade.php
+++ Enoun/resources/views/Contato/contato.blade.php
@@ -2,4 +2,18 @@
 @section('title','Contato')
 @section('conteudo')
 <h1>Contato</h1>
+<div class="container">
+<form action="" method="POST" name="cadastro" class="card">
+
+
+<label for="user">Usuário :</label>
+<input type="text" name="user" id="user">
+
+<label for="texto">Texto</label>
+<textarea name="texto" id="texto" cols="30" rows="10"></textarea>
+
+<button class="btn btn-info" type="submit" name="cadastro" value="Cadastro" >Enviar</button>
+
+</form>
+
 @endsection
