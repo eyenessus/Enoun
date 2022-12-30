da3cb731e28da8813e0c8fa68fe8008926f353d9
Emerson S <eyenessus@gmail.com>
2022-12-29 22:12:06 -0300

    HTML login, cadastro adicionado

--- Enoun/resources/views/Cadastro/cadastro.blade.php
+++ Enoun/resources/views/Cadastro/cadastro.blade.php
@@ -2,4 +2,28 @@
 @section('title','Cadastro')
 @section('conteudo')
 <h1>CADASTRO</h1>
+<div class="container">
+<form action="" method="POST" name="cadastro" class="card">
+
+
+<label for="user">Usuário :</label>
+<input type="text" name="user" id="user">
+
+<label for="pnome">Nome :</label>
+<input type="text" name="pnome" id="primeironome">
+
+<label for="snome">Sobrenome :</label>
+<input type="text" name="snome" id="sobrenome">
+
+<label for="nascimento">Data de nascimento :</label>
+<input type="date" name="nascimento" id="nascimento">
+
+<label for="password">Senha :</label>
+<input type="password" name="senha" id="senha">
+
+<button class="btn btn-info" type="submit" name="cadastro" value="Cadastro" >Cadastrar</button>
+
+</form>
+
+</div>
 @endsection
--- Enoun/resources/views/Login/login.blade.php
+++ Enoun/resources/views/Login/login.blade.php
@@ -2,4 +2,18 @@
 @section('title','Login')
 @section('conteudo')
 <h1>Login</h1>
+<div class="container">
+<form action="" method="POST" name="login" class="card">
+
+<label for="user">Usuário :</label>
+<input type="text" name="user" id="user">
+
+<label for="password">Senha :</label>
+<input type="password" name="senha" id="senha">
+
+<button class="btn btn-info" type="submit" name="login" value="Login" >Entrar</button>
+
+</form>
+
+</div>
 @endsection
