832e494677aee52f91878cfa8dfb3c2feae04d56
Emerson S <eyenessus@gmail.com>
2023-01-09 10:47:09 -0300

    correcao de textos

--- Enoun/resources/views/Servicos/resultado.blade.php
+++ Enoun/resources/views/Servicos/resultado.blade.php
@@ -2,12 +2,12 @@
 @section('titulo',$resultadoId->nome)
 @section('conteudo')
 <div class="container">
-  <h1 class="card-title text-uppercase">{{$resultadoId->nome}}</h1>
+  <h1 class="card-title text-info">{{$resultadoId->nome}}</h1>
   <div class="card mb-3">
     <img src="/img/publicserivces/{{$resultadoId->imagem}}" class="card-img-top" alt="...">
     <div class="card-body">
       <p class="card-text">{{$resultadoId->descricao}}</p>
-      <p class="card-text"><small class="text-muted">{{$resultadoId->categoria}}</small></p>
+      <p class="card-text text-uppercase"><small class="text-muted">{{$resultadoId->categoria}}</small></p>
     </div>
   </div>
 
