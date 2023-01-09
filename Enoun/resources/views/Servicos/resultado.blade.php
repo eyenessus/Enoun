2ba025856ea8eba3ee2a238f97ac5bf197a5de24
Emerson S <eyenessus@gmail.com>
2023-01-09 10:46:02 -0300

    melhoria de layout pagina de saiba mais

--- Enoun/resources/views/Servicos/resultado.blade.php
+++ Enoun/resources/views/Servicos/resultado.blade.php
@@ -2,21 +2,15 @@
 @section('titulo',$resultadoId->nome)
 @section('conteudo')
 <div class="container">
-
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="/img/publicserivces/{{$resultadoId->imagem}}" class="img-fluid rounded-start" alt="..." >
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{$resultadoId->nome}}</h5>
-        <p class="card-text">{{$resultadoId->descricao}} </p>
-        <p class="card-text"><small class="text-muted">{{$resultadoId->categoria}}</small></p>
-      </div>
+  <h1 class="card-title text-uppercase">{{$resultadoId->nome}}</h1>
+  <div class="card mb-3">
+    <img src="/img/publicserivces/{{$resultadoId->imagem}}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">{{$resultadoId->descricao}}</p>
+      <p class="card-text"><small class="text-muted">{{$resultadoId->categoria}}</small></p>
     </div>
   </div>
-</div>
+
 
 </div>
 
