1507c3e958911d1f17ee3d9129131c1835fd6287
Emerson S <eyenessus@gmail.com>
2023-01-09 10:03:32 -0300

    regras no form e novos campos json adicionado

--- Enoun/resources/views/Registro/service.blade.php
+++ Enoun/resources/views/Registro/service.blade.php
@@ -8,11 +8,11 @@
 @csrf
     <div class="mb-3">
   <label for="nome" class="form-label">Titulo do serviço</label>
-  <input type="text" class="form-control" id="nome" name="nome" placeholder="Titulo">
+  <input type="text" class="form-control" id="nome" name="nome" placeholder="Titulo" required>
 </div>
 <div class="mb-3">
   <label for="imagem" class="form-label">Imagem:</label>
-  <input type="file" class="form-control" id="imagem" name="imagem">
+  <input type="file" class="form-control" id="imagem" name="imagem" required>
 </div>
 <div class="mb-3">
   <label for="categoria" class="form-label">Categoria</label>
@@ -25,11 +25,37 @@
 </div>
 <div class="mb-3">
   <label for="codigo" class="form-label">Codigo</label>
-  <input type="text" class="form-control" id="codigo" name="codigo" placeholder="Código do serviço">
+  <input type="text" class="form-control" id="codigo" name="codigo" placeholder="Código do serviço" required>
 </div>
 <div class="mb-3">
   <label for="descricao" class="form-label">Descrição:</label>
-  <textarea class="form-control" id="descricao" name="descricao" rows="3"></textarea>
+  <textarea class="form-control" id="descricao" name="descricao" rows="3" required></textarea>
+</div>
+
+<div class="mb-3 form-check">
+  <input type="checkbox" class="form-check-input" id="atendimento" value="atende" name="inforextra[]" required>
+  <label class="form-check-label" for="atendimento">Atendimento prioritário</label>
+</div>
+
+<div class="mb-3 form-check">
+  <input type="checkbox" class="form-check-input" id="destaque" name="inforextra[]" required>
+  <label class="form-check-label" for="destaque">Em destaque</label>
+</div>
+
+
+<div class="mb-3 form-check">
+  <input type="checkbox" class="form-check-input" id="novo" value="novo" name="inforextra[]" required>
+  <label class="form-check-label" for="novo">Novo</label>
+</div>
+
+<div class="mb-3 form-check">
+  <input type="checkbox" class="form-check-input" id="gratuito" name="inforextra[]" required>
+  <label class="form-check-label" for="gratuito">Gratuito</label>
+</div>
+
+<div class="mb-3 form-check">
+  <input type="checkbox" class="form-check-input" id="pagamento" name="inforextra[]" required>
+  <label class="form-check-label" for="pagamento">Pagamento</label>
 </div>
 
 <div class="mb-3">
