5046c23bba8a3fe157bbf73457168a6d7dd2ca9b
Emerson S <eyenessus@gmail.com>
2023-02-11 20:16:39 -0300

    tabela de mensagem adicionado

---
+++ database/migrations/2023_02_11_231200_create_mensagens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMensagensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mensagens', function (Blueprint $table) {
+            $table->id();
+            $table->string('usuario');
+            $table->text('mensagem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mensagens');
+    }
+}
