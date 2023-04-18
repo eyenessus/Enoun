4662beb7aa9acdfbf636440736d8ddf20b95b205
Emerson S <eyenessus>
2023-04-17 22:31:12 -0300

    pix pagseguro funcionando perfeitamente

--- app/Http/Controllers/Pay/PagseguroController.php
+++ app/Http/Controllers/Pay/PagseguroController.php
@@ -89,7 +89,8 @@ class PagseguroController extends Controller
         dd($resposta->json());
     }
 
-    public function boleto(){
+    public function boleto()
+    {
         $data = [
             "reference_id" => "ex-00001",
             "customer" => [
@@ -164,15 +165,66 @@ class PagseguroController extends Controller
                 ]
             ]
         ];
-        
+
         $resposta = http::withToken('')->withHeaders([
             'Content-type' => 'application/json'
-        ])->post('https://sandbox.api.pagseguro.com/orders',$data);
+        ])->post('https://sandbox.api.pagseguro.com/orders', $data);
 
         dd($resposta->json());
     }
-    public function pix(){
+    public function pix()
+    {
+        $data = [
+            "reference_id" => "ex-00001",
+            "customer" => [
+                "name" => "Jose da Silva",
+                "email" => "email@test.com",
+                "tax_id" => "12345678909",
+                "phones" => [
+                    [
+                        "country" => "55",
+                        "area" => "11",
+                        "number" => "999999999",
+                        "type" => "MOBILE"
+                    ]
+                ]
+            ],
+            "items" => [
+                [
+                    "name" => "nome do item",
+                    "quantity" => 1,
+                    "unit_amount" => 500
+                ]
+            ],
+            "qr_codes" => [
+                [
+                    "amount" => [
+                        "value" => 500
+                    ],
+                    "expiration_date" => "2023-04-29T20:15:59-03:00"
+                ]
+            ],
+            "shipping" => [
+                "address" => [
+                    "street" => "Avenida Brigadeiro Faria Lima",
+                    "number" => "1384",
+                    "complement" => "apto 12",
+                    "locality" => "Pinheiros",
+                    "city" => "São Paulo",
+                    "region_code" => "SP",
+                    "country" => "BRA",
+                    "postal_code" => "01452002"
+                ]
+            ],
+            "notification_urls" => [
+                "https://meusite.com/notificacoes"
+            ]
+        ];
 
+        $resposta = http::withToken('')->withHeaders([
+            'Content-type' => 'application/json'
+        ])->post('https://sandbox.api.pagseguro.com/orders', $data);
+
+        dd($resposta->json());
     }
-    
 }
