e28b22e9ff8a192c2114f2b62ec24ed914ee2066
Emerson S <eyenessus>
2023-06-01 00:26:58 -0300

    jwt recebendo requisicoes e devolvendo token

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthControllerApi;
 use App\Http\Controllers\Api\ProdutosControllerApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('produtos',[ProdutosControllerApi::class,'index']);
+Route::post('auth/login',[AuthControllerApi::class,'login']);
