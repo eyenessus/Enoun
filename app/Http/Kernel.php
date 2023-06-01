9a6f8598f5bee353b2889d1b8eb2b04aab7464b6
Emerson S <eyenessus>
2023-06-01 00:47:44 -0300

    controle de api pelo middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
         ],
     ];
 
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'identificacao' => \App\Http\Middleware\Identidade::class,
         'admin' => \App\Http\Middleware\AdminAcess::class,
+        'apiJwt' => \App\Http\Middleware\ApiProtectedRoute::class
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\Api\Auth\AuthControllerApi;
 use App\Http\Controllers\Api\ProdutosControllerApi;
+use App\Http\Controllers\Api\UserControllerApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('produtos',[ProdutosControllerApi::class,'index']);
+Route::get('users',[UserControllerApi::class,'index'])->middleware('apiJwt');
 Route::post('auth/login',[AuthControllerApi::class,'login']);
